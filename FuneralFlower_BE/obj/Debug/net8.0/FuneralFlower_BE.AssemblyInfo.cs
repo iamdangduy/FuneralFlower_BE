320bc000e0d36ab4c8e48ece6ca487e28b5fb120
Nguyễn Đăng Duy <nguyendangduy18082001@gmail.com>
2025-04-10 23:41:13 +0700

    1

--- FuneralFlower_BE/obj/Debug/net8.0/FuneralFlower_BE.AssemblyInfo.cs
+++ FuneralFlower_BE/obj/Debug/net8.0/FuneralFlower_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FuneralFlower_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0dc666f4c66d8337ab7d8c11f5f4a5875d1e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f91fd636d80a5183df032f740f6294bdeef218")]
 [assembly: System.Reflection.AssemblyProductAttribute("FuneralFlower_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FuneralFlower_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
