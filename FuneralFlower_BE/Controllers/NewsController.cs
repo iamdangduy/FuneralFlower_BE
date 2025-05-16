55679991733056cfcea1a704068d21a2d4f6fcc3
Duy Nguyen <nguyendangduy18082001@gmail.com>
2025-05-16 09:43:34 +0700

    Update NewsController.cs

--- FuneralFlower_BE/Controllers/NewsController.cs
+++ FuneralFlower_BE/Controllers/NewsController.cs
@@ -69,4 +69,51 @@ public class NewsController : BaseController
             return Error(ex.Message);
         }
     }
+
+    [HttpPost]
+    public JsonResponse UpdateProduct(News model)
+    {
+        try
+        {
+            UserService userService = new UserService();
+            //string token = Request.Headers.Authorization.ToString();
+            //User? user = userService.GetUserByToken(token);
+            //if (user == null) return Unauthorized();
+            NewsService newsService = new NewsService();
+            var oldNews = newsService.GetById(model.Id);
+
+            oldNews.Title = model.Title;
+            if (!string.IsNullOrEmpty(model.TitleImageUrl))
+            {
+                string filename = Guid.NewGuid().ToString() + ".jpg";
+                var path = _hostingEnvironment.WebRootPath + Constant.PATH.NEWS_IMAGE_PATH + filename;
+                HelperProvider.Base64ToImage(model.TitleImageUrl, path);
+                //if (!HelperProvider.DeleteFile(oldProduct.ProductImageUrl, _hostingEnvironment)) return Error();
+                oldNews.TitleImageUrl = Constant.PATH.NEWS_IMAGE_URL + filename;
+            }
+            oldNews.NewsContent = model.NewsContent;
+            newsService.UpdateNews(oldNews);
+
+            return Success();
+        }
+        catch (Exception ex)
+        {
+            return Error(ex.Message);
+        }
+    }
+
+    [HttpGet]
+    public JsonResponse DeleteNews(string Id)
+    {
+        try
+        {
+            NewsService newsService = new NewsService();
+            newsService.DeleteNews(Id);
+            return Success(null, "Xoá dữ liệu thành công!");
+        }
+        catch (Exception ex)
+        {
+            return Error(ex.Message);
+        }
+    }
 }
